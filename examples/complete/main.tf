0d0904c70a89e351663b8eeae4dc3f09d76c1b93
shanye <shanye.xzq@alibaba-inc.com>
2025-10-15 19:49:48 +0800

    fix: improve the examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,17 +1,27 @@
+provider "alicloud" {
+  region = "ap-southeast-5"
+}
+
 data "alicloud_db_zones" "default" {
   engine         = "MySQL"
   engine_version = "5.6"
 }
 
+locals {
+  zone_id = data.alicloud_db_zones.default.zones[0].id
+}
+
 data "alicloud_images" "default" {
-  name_regex = "^centos_6"
+  owners        = "system"
+  most_recent   = true
+  instance_type = data.alicloud_instance_types.default.instance_types[0].id
 }
 
 data "alicloud_instance_types" "default" {
-  availability_zone    = data.alicloud_db_zones.default.zones[0].id
+  availability_zone    = local.zone_id
   cpu_core_count       = 2
   memory_size          = 8
-  instance_type_family = "ecs.g6"
+  instance_type_family = "ecs.g9i"
 }
 
 data "alicloud_db_instance_classes" "default" {
@@ -26,7 +36,7 @@ module "example" {
   name              = var.name
   vpc_cidr_block    = "172.16.0.0/16"
   vs_cidr_block     = "172.16.0.0/21"
-  availability_zone = data.alicloud_db_zones.default.zones[0].id
+  availability_zone = local.zone_id
 
   #alicloud_slb_load_balancer
   slb_address_type = "intranet"
@@ -35,14 +45,14 @@ module "example" {
 
   #alicloud_instance
   instance_type              = data.alicloud_instance_types.default.instance_types[0].id
-  system_disk_category       = "cloud_efficiency"
+  system_disk_category       = "cloud_essd"
   system_disk_name           = var.system_disk_name
   system_disk_description    = var.system_disk_description
   image_id                   = data.alicloud_images.default.images[0].id
   internet_max_bandwidth_out = var.internet_max_bandwidth_out
   ecs_size                   = 1200
   data_disks_name            = "data_disks_name"
-  category                   = "cloud_efficiency"
+  category                   = "cloud_essd"
   description                = "tf-vpc-ecs-description"
   encrypted                  = true
 
